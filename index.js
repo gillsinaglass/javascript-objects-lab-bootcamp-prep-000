a34380586adf289efb7e681bb4874688eb13c345
Ryan Gill <rdgill95@gmail.com>
2018-09-13 04:03:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function deleteFromObjectByKey(object,key){
  delete newObj.key
  return newObj
 }
-function destrucctivelyDeleteFromObjectByKey(object,key){
+function destructivelyDeleteFromObjectByKey(object,key){
   delete object.key;
   return object
 }
