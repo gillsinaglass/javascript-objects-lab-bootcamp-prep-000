2a7e5dd3ea3d554e802780b16834b52157427a94
Ryan Gill <rdgill95@gmail.com>
2018-09-13 04:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var recipes = {};
 };
  function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
-  delete newObj.key
+  return newObj
+  delete newObj.key;
   return newObj
 };
  function destructivelyDeleteFromObjectByKey(object, key) {
